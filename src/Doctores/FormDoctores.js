b1e2d5cb8e316127042bd3b07d3c7cd9b6835686
Cesar Fajardo <cfajardoramos@gmail.com>
2022-11-16 22:48:10 -0500

    Modificacion pantalla de doctores

--- src/Doctores/FormDoctores.js
+++ src/Doctores/FormDoctores.js
@@ -1,29 +1,67 @@
+import { useState } from "react";
+
 const FormDoctores = () => {
+    
+    const [doctor, setDoctor] = useState({
+        c: ''
+    });
+
+
+    const crearDoctor = async () => {
+        const data = {
+            nombre: doctor.nombres
+        }
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        crearDoctor();
+    }
+
     return (
         <div className="container mt-3">
             <h3>Registro de Doctores</h3>
-            <form className="container" action="">
+            <form className="container" onSubmit={onSubmit}>
                 <div className="row">
                     <div className="col-4">
-                        <label htmlFor="nombrecompleto">Nombres completos *</label>
+                        <label htmlFor="nombre">Nombres *</label>
                         <input className="form-control" type="text" name="nombrecompleto" id="nombrecompleto" required/>
                     </div>
                     <div className="col-4">
-                        <label htmlFor="tipo">Seleccione documento *</label>
+                        <label htmlFor="apellidos">Apellidos *</label>
+                        <input className="form-control" type="text" name="nombrecompleto" id="nombrecompleto" required/>
+                    </div>
+                    <div className="col-4">
+                        <label htmlFor="documento">Documento *</label>
+                        <input className="form-control" type="text" name="nombrecompleto" id="nombrecompleto" required/>
+                    </div>
+                    <div className="col-4">
+                        <label htmlFor="especialidad">Especialidad *</label>
                         <select className="form-select" type="text" name="tipo" id="tipo" required>
                             <option value=""></option>
-                            <option value="Cedula">Cedula</option>
-                            <option value="Cedula extranjería">Cedula extranjería</option>
-                            <option value="Pasaporte">Pasaporte</option>
+                            <option value="Medico General">Medico General</option>
+                            <option value="Ginecologo">Ginecologo</option>
+                            <option value="Pediatra">Pediatra</option>
                         </select>
                     </div>
                     <div className="col-4">
-                        <label htmlFor="documento">Ingrese documento *</label>
-                        <input className="form-control" type="text" name="documento" id="documento" placeholder="Ingrese documento"/>
+                        <label htmlFor="direccion">Dirección *</label>
+                        <input className="form-control" type="text" name="nombrecompleto" id="nombrecompleto" required/>
                     </div>
+                    <div className="col-4">
+                        <label htmlFor="telefono">Telefono *</label>
+                        <input className="form-control" type="text" name="nombrecompleto" id="nombrecompleto" required/>
+                    </div>
+                    <div className="col-4">
+                        <label htmlFor="correo">Correo *</label>
+                        <input className="form-control" type="text" name="nombrecompleto" id="nombrecompleto" required/>
+                    </div>
+                    
                 </div>
                 <div className="row">
-                    
+                    <div className="d-flex flex-row-reverse">
+                        <button type="submit" className="btn btn-primary">Guardar</button>
+                    </div>
                 </div>
             </form>
         </div>
--- src/Doctores/ListadoDoctores.js
+++ src/Doctores/ListadoDoctores.js
@@ -1,28 +1,72 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import APIInvoke from "../utils/APIInvoke";
+
 const ListadoDoctores = () => {
+
+    
+    const [doctores, setDoctores] = useState([]);
+    
+    const cargarDoctores = async () => {
+        const response = await APIInvoke.invokeGET(`/doctores`);
+        console.log(response);
+        setDoctores(response);
+    }
+
+    useEffect(() => {
+        cargarDoctores();
+    }, [])
+
     return (
         <div className="container">
-            <h3 className="mt-3">Lista de Doctores</h3>
-            <table className="table table-sm"> 
-                <thead>
-                    <tr>
-                        <th>Nombres completos</th>
-                        <th>Documento</th>
-                        <th>Telefono</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Carlos Palma</td>
-                        <td>12345</td>
-                        <td>32165</td>
-                        <td>
-                            <button>Editar</button>
-                            <button>Eliminar</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="card mt-3">
+                <div className="card-header">
+                    <h3 className="card-title d-flex">
+                        <div style={{flex: 1}}>
+                            Lista de Doctores
+                        </div>
+
+                        <Link to={`/doctores/form`} className="btn btn-block btn-primary btn-sm">Registrar Doctor</Link>
+                    </h3>
+                </div>
+                <div className="card-body">
+                    <table className="table table-sm"> 
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellidos</th>
+                                <th>Documento</th>
+                                <th>Especialidad</th>
+                                <th>Dirección</th>
+                                <th>Telefono</th>
+                                <th>Correo</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                doctores.map(
+                                    item =>
+                                        <tr key={item._id}>
+                                            <td>{item.nombres}</td>
+                                            <td>{item.apellidos}</td>
+                                            <td>{item.documento}</td>
+                                            <td>{item.especialidad}</td>
+                                            <td>{item.direccion}</td>
+                                            <td>{item.telefono}</td>
+                                            <td>{item.correo}</td>
+                                            <td>
+                                                <Link to={`/tareas-editar/${item._id}`} className="btn btn-sm btn-primary">Editar</Link>&nbsp;&nbsp;
+                                                <button className="btn btn-sm btn-danger">Borrar</button>
+                                            </td>
+                                        </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
